884856190d03f243b354b07e7a33e83bf1dd34ad
Quang Minh Trần <tranquangminhduclap@gmail.com>
2024-08-11 10:01:42 +0700

    big update

---
+++ user_data.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE user_data;
+
+USE user_data;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
