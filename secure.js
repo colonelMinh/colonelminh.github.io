071abbc548c380c59638c2fdad03b876b6c4c411
Quang Minh Trần <tranquangminhduclap@gmail.com>
2024-12-20 22:19:23 +0700

    Big change

--- secure.js
+++ secure.js
@@ -17,11 +17,17 @@ function topFunction() {
     var correctPassword = "minhsiucapvjppro"; 
     var pass = "minhcancutethe:))";
     var pass1 = "lionelmessithegoat";
+    var userKey = "Admin"; // Key cho tài khoản admin
 
     function checkPassword() {
         var userPassword = prompt("Vui lòng nhập key để xem nội dung trang web:");
-        if (userPassword === correctPassword || userPassword === pass || userPassword === pass1) {
-            alert("Key chính xác! Chào mừng bạn!");
+        if (userPassword === correctPassword || userPassword === pass || userPassword === pass1 || userPassword === userKey) {
+            if (userPassword === userKey) {
+                window.location.href = "edit.html";
+            } else {
+                alert("Key chính xác! Chào mừng bạn!");
+                localStorage.setItem('user', JSON.stringify({ key: userKey }));
+            }
         } else {
             alert("Key không chính xác. Liên hệ tại https://forms.gle/F94L36wYKzaHR8sTA để lấy key nếu chưa có). Trước khi lấy key, đăng ký tại https://colonelminh.github.io/signup.html. Trang web sẽ đóng lại.");
             window.location.href = "https://www.google.com/";
